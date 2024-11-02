5e3d30db9ae07a61c832125d28f5c810fe2f72d7
kundathilnithi <kundathil.nithi@gmail.com>
2024-11-02 17:10:51 +0530

    backend config

--- vars/generateConfig.groovy
+++ vars/generateConfig.groovy
@@ -8,46 +8,13 @@ def templateFile = libraryResource('config.template')
 echo "$templateFile"
  def engine = new SimpleTemplateEngine()
      def binding = [
-        bucket    : "${params.bucket}",
+        bucket: "${params.bucket}",
         key: "${params.key}",
-        region     : "${params.region}"
+        region: "${params.region}"
     ]
    def template = engine.createTemplate(templateFile)
    def configContent = template.make(binding).toString()
    return "${configContent}"  
-   //def content = "Hello"
-  
-   // Write the populated backend configuration to a temporary file
-  //  writeFile file: 'backend.tf', text: "${configContent}, encoding: "UTF-8"
    
 } 
 
-// def call(Map params) {
-//     // Check if required parameters are provided
-//     if (!params.appName || !params.environment || !params.region) {
-//         error("Missing required parameters: appName, environment, region")
-//     }
-
-//     // Read the template file from the resources
-//     def templateFile = libraryResource('config.template')
-
-//     // Create a new SimpleTemplateEngine instance
-//     def engine = new SimpleTemplateEngine()
-
-//     // Create a binding with the parameters
-//     def binding = [
-//         appName    : 'myapp',
-//         environment: 'dev',
-//         region     : 'us-east1'
-//     ]
-
-//     // Create the template and replace the placeholders with values
-//     def template = engine.createTemplate(templateFile)
-//     def configContent = template.make(binding).toString()
-
-//     // Write the populated configuration to a new file
-//     writeFile file: 'config.properties', text: configContent
-
-//     // Optionally print the generated config for debugging
-//     echo "Generated config:\n${configContent}"
-// }
